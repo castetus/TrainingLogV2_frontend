6eeebba62f6ac15f1f037efd49ddcbd661f4b564
castetus <kboikov@mail.ru>
2026-06-07 11:12:22 +0200

    add exercise mutations

--- src/modules/Exercise/ExerciseForm.tsx
+++ src/modules/Exercise/ExerciseForm.tsx
@@ -1,12 +1,17 @@
 import { TextField, Select, MenuItem, Stack, type SelectChangeEvent, Button } from "@mui/material";
 import type { ExerciseForm } from "./Exercise.types";
-import { useEffect, useState, type ChangeEvent } from "react";
+import { useEffect, useState, type ChangeEvent, type FormEvent } from "react";
 import { getInitialExercise } from "./Exercise.utils";
 import { ExerciseType } from "@/shared/enums";
-import { useExercise } from "@/hooks/useExercise";
+import { useExercise, useCreateExercise, useUpdateExercise } from "@/api/exercises/exercises.queries";
+import { useNavigate } from "react-router";
+import { routes } from "@/app/routes";
 
 export default function ExerciseForm ({ id }: { id?: string }) {
-  const { exercise } = useExercise(id);
+  const navigate = useNavigate();
+  const { data: exercise } = useExercise(id);
+  const createMutation = useCreateExercise();
+  const updateMutation = useUpdateExercise();
 
   useEffect(() => {
     if (!exercise) return;
@@ -27,8 +32,34 @@ export default function ExerciseForm ({ id }: { id?: string }) {
     }));
   };
 
+  const canSubmit = form.name.trim().length > 0;
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!canSubmit) {
+      return;
+    }
+
+    try {
+      if (id) {
+        await updateMutation.mutateAsync({
+          id,
+          payload: form,
+        });
+      } else {
+        await createMutation.mutateAsync(form);
+      }
+
+      navigate(routes.exercises);
+    } catch (error) {
+      console.error(error);
+      // показать ошибку пользователю
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={(e) => handleSubmit(e)}>
       <Stack spacing={2}>
       <TextField
         value={form.name}
@@ -60,7 +91,7 @@ export default function ExerciseForm ({ id }: { id?: string }) {
           rows={4}
         />
 
-        <Button variant="contained">
+        <Button variant="contained" type="submit">
           Save
         </Button>
       </Stack>
